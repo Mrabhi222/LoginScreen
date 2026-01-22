0d1f9e5581ad7bbd8964cdcb8771bd20b8da18db
Mrabhi222 <abhisonawane444@gmail.com>
2026-01-22 13:57:59 +0530

    Numpad Design part

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using LoginScreen.ViewModel;
 using System.Reflection.PortableExecutable;
 
+
 namespace LoginScreen
 {
     public partial class MainPage : ContentPage
@@ -11,22 +12,23 @@ namespace LoginScreen
         {
             InitializeComponent();
             BindingContext = new LoginViewModel();
-            //Header.MenuTapped += OnHeaderMenuTapped;
+            
         }
 
-        private async void OnHeaderMenuTapped(object? sender, EventArgs e)
-        {
-            if (!isSidebarOpen)
-            {
-                await SideBar.TranslateTo(0, 0, 250, Easing.CubicOut);
-                isSidebarOpen = true;
-            }
-            else
-            {
-                await SideBar.TranslateTo(260, 0, 250, Easing.CubicIn);
-                isSidebarOpen = false;
-            }
-        }
+        //private async void OnHeaderMenuTapped(object? sender, EventArgs e)
+        //{
+        //    if (!isSidebarOpen)
+        //    {
+        //        await SideBar.TranslateTo(0, 0, 250, Easing.CubicOut);
+        //        isSidebarOpen = true;
+        //    }
+        //    else
+        //    {
+        //        await SideBar.TranslateTo(260, 0, 250, Easing.CubicIn);
+        //        isSidebarOpen = false;
+        //    }
+        //}
+
     }
 
 }
--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,18 +1,50 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using System.Text;
 
 namespace LoginScreen.ViewModel
 {
-     public partial class LoginViewModel : ObservableObject
+    public partial class LoginViewModel : ObservableObject
     {
+        // PIN
         [ObservableProperty]
         private string pin = string.Empty;
 
+        // VISIBILITY
+        [ObservableProperty]
+        private bool isNumpadVisible = true;
 
+        [ObservableProperty]
+        private bool isGifVisible = false;
+
+        [ObservableProperty]
+        private bool isAccessCard = false;
+
+        // LOGIN METHOD COMMANDS
+        [RelayCommand]
+        private void SelectUserCode()
+        {
+            IsNumpadVisible = true;
+            IsAccessCard = false;
+            IsGifVisible = false;
+        }
+
+        [RelayCommand]
+        private void SelectFingerprint()
+        {
+            IsNumpadVisible = false;
+            IsAccessCard = false;
+            IsGifVisible = true;
+        }
+
+        [RelayCommand]
+        private void SelectAccessCard()
+        {
+            IsNumpadVisible = false;
+            IsGifVisible = false;
+            IsAccessCard = true;
+        }
+
+        // KEYPAD
         [RelayCommand]
         private void KeyPressed(string key)
         {
@@ -21,17 +53,17 @@ namespace LoginScreen.ViewModel
                 if (Pin.Length > 0)
                     Pin = Pin[..^1];
             }
-            else if (Pin.Length < 4)   // PIN limit
+            else if (Pin.Length < 4)
             {
                 Pin += key;
             }
         }
 
+        // LOGIN
         [RelayCommand]
         private void Login()
         {
             Console.WriteLine($"PIN entered: {Pin}");
         }
-
     }
 }
